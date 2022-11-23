6b4d56cd06d5e2a412368c642a737de7e12deb34
darkzense <torresjohn094@gmail.com>
2022-11-22 23:35:47 -0600

    restructuring and semantic changes
    - better creation hierarchy
    - change all non-constraint statements to lowercase
    - add some missing empty lines in table creations
    - remove all other unrelated queries

--- Pharmatic.sql
+++ Pharmatic.sql
@@ -1,8 +1,89 @@
 create database pharmatic
 use pharmatic
 
+create table Categoria (
+
+	idCategoria int IDENTITY(1,1) PRIMARY KEY,
+
+	nombre nvarchar(30) UNIQUE NOT NULL
+
+)
+
+create table Producto (
+
+	idProducto int IDENTITY(1,1) PRIMARY KEY,
+
+	idCategoria int FOREIGN KEY REFERENCES Categoria(idCategoria) NOT NULL,
+
+	nombre nvarchar(50) UNIQUE NOT NULL,
+
+	descripcion nvarchar(150)
+
+)
+
+create table Medida (
+
+	idMedida int IDENTITY(1,1) PRIMARY KEY,
+
+	unidad nvarchar(30) UNIQUE NOT NULL
+
+)
+
+create table Contenido (
+
+	idContenido int IDENTITY(1,1) PRIMARY KEY,
+
+	idMedida int FOREIGN KEY REFERENCES Medida(idMedida) NOT NULL,
+
+	cantidad float NOT NULL
+	CHECK(cantidad > 0)
+
+)
+
+create table Proveedor (
+
+	idProveedor int IDENTITY(1,1) PRIMARY KEY NOT NULL,
+
+	nombre nvarchar(30) UNIQUE NOT NULL,
+
+	correo nvarchar(30) UNIQUE,
+
+	direccion nvarchar(50),
+
+	telefono int UNIQUE
+	CHECK(telefono >= 10000000 AND telefono <= 99999999),
+
+)
+
+create table Lote (
+
+	idLote int IDENTITY(1,1) PRIMARY KEY,
+
+	idProducto int FOREIGN KEY REFERENCES Producto(idProducto) NOT NULL,
+
+	idProveedor int FOREIGN KEY REFERENCES Proveedor(idProveedor),
+
+	idContenido int FOREIGN KEY REFERENCES Contenido(idContenido) NOT NULL,
+
+	enConsigna bit NOT NULL,
+
+	fechaIngreso date,
+
+	fechaVencimiento date NOT NULL,
+
+	precioCompra money
+	CHECK(precioCompra > 0),
+
+	precioVenta money NOT NULL
+	CHECK(precioVenta > 0),
+
+	stock int NOT NULL
+	CHECK(stock >= 0)
+
+)
+
 /*
-Descripci�n:
+Descripcion:
 	La farmacia necesita poder registrar los datos de sus clientes, por lo que la tabla
 	registra datos de contacto relacionado al cliente.
 
@@ -15,19 +96,17 @@ Descripci�n:
 	:-----------+-----------------+------------------------------------------------:
 	| apellidos | Doe Ipsum       | Not null                                       |
 	:-----------+-----------------+------------------------------------------------:
-	| correo    | john.d@mail.com | �nico, debe contener @ y .                     |
+	| correo    | john.d@mail.com | Unico, debe contener @ y .                     |
 	:-----------+-----------------+------------------------------------------------:
-	| telefono  | 83481615        | �nico, debe contener 8 d�gitos                 |
+	| telefono  | 83481615        | Unico, debe contener 8 digitos                 |
 	:-----------+-----------------+------------------------------------------------:
-	| sexo      | M               | Debe ser un car�cter M o F                     |
+	| sexo      | M               | Debe ser un caracter M o F                     |
 	:-----------+-----------------+------------------------------------------------:
 	| edad      | 23              | Debe ser un numero mayor que 0 y menor que 150 |
 	'-----------'-----------------'------------------------------------------------'
 	
 */
-drop table Cliente
-
-CREATE TABLE Cliente (
+create table Cliente (
 
 	idCliente int IDENTITY(1,1) PRIMARY KEY,
 
@@ -39,7 +118,7 @@ CREATE TABLE Cliente (
 	CHECK(correo LIKE '%__@__%.__%'),
 
 	telefono int UNIQUE
-	CHECK(telefono <= 99999999 AND telefono >= 10000000),
+	CHECK(telefono >= 10000000 AND telefono <= 99999999),
 
 	sexo char(1) NOT NULL
 	CHECK(sexo = 'M' OR sexo = 'F'),
@@ -48,11 +127,6 @@ CREATE TABLE Cliente (
 	CHECK(edad > 0 AND edad < 150)
 
 )
-
-select * from Cliente
-
-insert into Cliente(idCliente,nombres,apellidos,correo,telefono,sexo,edad)
-values(1,'joni','gei','okay@mail.es','12345678','f',9)
 
 go
 -- Retorna el subtotal de una factura dado su id
@@ -94,11 +168,7 @@ begin
 end
 go
 
-/*
-CONTEXTO:
-
-*/
-CREATE TABLE Factura (
+create table Factura (
 
 	idFactura int IDENTITY(1,1) PRIMARY KEY,
 
@@ -115,99 +185,6 @@ CREATE TABLE Factura (
 
 	total as dbo.calcTotalFactura(idFactura)
 
-)
-
-create table Categoria (
-
-	idCategoria int IDENTITY(1,1) PRIMARY KEY,
-
-	nombre nvarchar(30) UNIQUE NOT NULL
-
-)
-
-create table Tag (
-
-	idTag int IDENTITY(1,1) PRIMARY KEY,
-
-	nombre nvarchar(30) UNIQUE NOT NULL
-)
-
-create table Medida (
-
-	idMedida int IDENTITY(1,1) PRIMARY KEY,
-
-	unidad nvarchar(30) UNIQUE NOT NULL
-)
-
-create table Contenido (
-
-	idContenido int IDENTITY(1,1) PRIMARY KEY,
-
-	idMedida int FOREIGN KEY REFERENCES Medida(idMedida) NOT NULL,
-
-	cantidad float NOT NULL
-	CHECK(cantidad > 0)
-)
-
-create table Producto (
-
-	idProducto int IDENTITY(1,1) PRIMARY KEY,
-
-	idCategoria int FOREIGN KEY REFERENCES Categoria(idCategoria) NOT NULL,
-
-	nombre nvarchar(50) UNIQUE NOT NULL,
-
-	descripcion nvarchar(150)
-
-)
-
-create table TagsProducto (
-
-	idTag int FOREIGN KEY REFERENCES Tag(idTag) NOT NULL,
-
-	idProducto int FOREIGN KEY REFERENCES Producto(idProducto) NOT NULL
-
-)
-
-create table Proveedor (
-
-	idProveedor int IDENTITY(1,1) PRIMARY KEY NOT NULL,
-
-	nombre nvarchar(30) UNIQUE NOT NULL,
-
-	correo nvarchar(30) UNIQUE,
-
-	direccion nvarchar(50),
-
-	telefono int UNIQUE
-	CHECK(telefono <= 99999999 AND telefono >= 10000000),
-
-)
-
-create table Lote (
-
-	idLote int IDENTITY(1,1) PRIMARY KEY,
-
-	idProducto int FOREIGN KEY REFERENCES Producto(idProducto) NOT NULL,
-
-	idProveedor int FOREIGN KEY REFERENCES Proveedor(idProveedor),
-
-	idContenido int FOREIGN KEY REFERENCES Contenido(idContenido) NOT NULL,
-
-	enConsigna bit NOT NULL,
-
-	fechaIngreso date,
-
-	fechaVencimiento date NOT NULL,
-
-	precioCompra money
-	CHECK(precioCompra > 0),
-
-	precioVenta money NOT NULL
-	CHECK(precioVenta > 0),
-
-	stock int NOT NULL
-	CHECK(stock >= 0)
 )
 
 go
@@ -239,4 +216,18 @@ create table Venta (
 
 )
 
+create table Tag (
 
+	idTag int IDENTITY(1,1) PRIMARY KEY,
+
+	nombre nvarchar(30) UNIQUE NOT NULL
+
+)
+
+create table TagsProducto (
+
+	idTag int FOREIGN KEY REFERENCES Tag(idTag) NOT NULL,
+
+	idProducto int FOREIGN KEY REFERENCES Producto(idProducto) NOT NULL
+
+)
