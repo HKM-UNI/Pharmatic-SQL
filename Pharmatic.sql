48ec3d64c197614baa04a1d32606a92bc0a00eb7
darkzense <torresjohn094@gmail.com>
2022-11-22 22:01:07 -0600

    let the system name the constraints
    constraints are not hints for programmers

--- Pharmatic.sql
+++ Pharmatic.sql
@@ -36,19 +36,15 @@ CREATE TABLE Cliente (
 	apellidos nvarchar(30) NOT NULL,
 
 	correo nvarchar(30) UNIQUE
-	CONSTRAINT [Verifique el formato del correo. ej: 'user@mail.com']
 	CHECK(correo LIKE '%__@__%.__%'),
 
 	telefono int UNIQUE
-	CONSTRAINT [Verifique el formato del n�mero celular. ej: '80808080']
 	CHECK(telefono <= 99999999 AND telefono >= 10000000),
 
 	sexo char(1) NOT NULL
-	CONSTRAINT [Verifique el sexo. ej: 'M' o 'F']
 	CHECK(sexo = 'M' OR sexo = 'F'),
 
 	edad int
-	CONSTRAINT [Verifique que la edad sea v�lida]
 	CHECK(edad > 0 AND edad < 150)
 
 )
@@ -58,7 +54,6 @@ select * from Cliente
 insert into Cliente(idCliente,nombres,apellidos,correo,telefono,sexo,edad)
 values(1,'joni','gei','okay@mail.es','12345678','f',9)
 
-drop table
 /*
 CONTEXTO:
 
@@ -68,23 +63,19 @@ CREATE TABLE Factura (
 	idFactura int IDENTITY(1,1) PRIMARY KEY,
 
 	subTotal money NOT NULL
-	CONSTRAINT [El subtotal no puede ser cero o negativo]
 	CHECK(subTotal > 0),
 
 	subIva money NOT NULL
-	CONSTRAINT [El subIva no puede ser negativo]
 	CHECK(subTotal >= 0),
 
 	fecha timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
 
 	descuento float NOT NULL
-	CONSTRAINT [El descuento no puede ser negativo]
 	CHECK(descuento >= 0),
 
 	idCliente int FOREIGN KEY REFERENCES Cliente(idCliente),
 
 	total money NOT NULL
-	CONSTRAINT [El total no puede ser negativo o cero]
 	CHECK(total > 0)
 
 )
@@ -118,7 +109,6 @@ create table Contenido (
 	idMedida int FOREIGN KEY REFERENCES Medida(idMedida) NOT NULL,
 
 	cantidad float NOT NULL
-	CONSTRAINT [La cantidad no puede ser negativa o cero]
 	CHECK(cantidad > 0)
 )
 
@@ -153,7 +143,6 @@ create table Proveedor (
 	direccion nvarchar(50),
 
 	telefono int UNIQUE
-	CONSTRAINT [Verifique el formato del n�mero celular. ej: '80808080']
 	CHECK(telefono <= 99999999 AND telefono >= 10000000),
 
 )
@@ -183,15 +172,12 @@ create table Lote (
 	fechaVencimiento date NOT NULL,
 
 	precioCompra money
-	CONSTRAINT [El precioCompra no puede ser negativo o cero]
 	CHECK(precioCompra > 0),
 
 	precioVenta money NOT NULL
-	CONSTRAINT [El precioVenta no puede ser negativo o cero]
 	CHECK(precioVenta > 0),
 
 	stock int NOT NULL
-	CONSTRAINT [El stock no puede ser negativo]
 	CHECK(stock >= 0)
 )
 
@@ -204,11 +190,9 @@ create table Venta (
 	idLote int FOREIGN KEY REFERENCES Lote(idLote) NOT NULL,
 
 	cantidad int DEFAULT(1) NOT NULL
-	CONSTRAINT [la cantidad no puede ser negativo o cero]
 	CHECK(cantidad > 0),
 
 	monto money NOT NULL
-	CONSTRAINT [El monto no puede ser negativo o cero]
 	CHECK(cantidad > 0)
 
 )
