94f3fd23463cbc0bfafebe53d560fbc53fadd6a8
darkzense <torresjohn094@gmail.com>
2022-11-24 11:53:56 -0600

    add mock data for `Tag`'s

--- MockData/9. Tag (Opcional).sql
+++ MockData/9. Tag (Opcional).sql
@@ -1,1 +1,21 @@
--- TODO
+/*
+    17 Tags
+*/
+use pharmatic
+insert into Tag values ('dolor')
+insert into Tag values ('cabeza')
+insert into Tag values ('piel')
+insert into Tag values ('resfriado')
+insert into Tag values ('gripe')
+insert into Tag values ('estomago')
+insert into Tag values ('fiebre')
+insert into Tag values ('tos')
+insert into Tag values ('ojos')
+insert into Tag values ('zika')
+insert into Tag values ('covid')
+insert into Tag values ('dengue')
+insert into Tag values ('alergia')
+insert into Tag values ('ardor')
+insert into Tag values ('presion')
+insert into Tag values ('neumonia')
+insert into Tag values ('traumatismo')
